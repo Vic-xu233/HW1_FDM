e5bf74db44fdd99c87067ff481163d8775da887c
Kiwi Xu <920650757@qq.com>
2025-03-26 20:52:40 +0800

    adjust plt

--- hw1test.py
+++ hw1test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-flag=4  ##选择对哪个函数进行差分
+flag=1  ##选择对哪个函数进行差分
 k=2
 
 def u(x):##函数
@@ -45,14 +45,6 @@ def c_diff(x,dx):                    ##中间差分，二阶精度
     u_cdiff=(u(x+dx)-u(x-dx))/(2*dx)
     return u_cdiff
 
-def tri_c_diff(x,dx):         ##四格点  三阶精度
-    xi_1=x+dx
-    xi_2=x+2*dx
-    xi_n1=x-dx
-    u_tcdiff=-u(xi_2)+6*u(xi_1)-3*u(x)-2*u(xi_n1)
-    u_tcdiff=u_tcdiff/(6*dx)
-    return u_tcdiff
-
 def double_diff(x,dx):         ##三格点 二阶精度
     u_double_diff=(u(x+dx)-2*u(x)+u(x-dx))/(dx*dx)
     return u_double_diff
@@ -74,24 +66,25 @@ def cal(x, deltax):
     for dx in deltax:
         u1_real = u_rdiff(x)
         u2_real = u_2rdiff(x)
-        errors_cdiff.append(c_diff(x, dx) - u1_real)
-        errors_fdiff.append(f_diff(x, dx) - u1_real)
-        errors_2cdiff.append(double_diff(x, dx) - u2_real)
-        errors_2fdiff.append(dou_tri_diff(x, dx) - u2_real)
+        errors_cdiff.append(c_diff(x, dx) - u1_real)               #2阶
+        errors_fdiff.append(f_diff(x, dx) - u1_real)               #1阶
+        errors_2cdiff.append(double_diff(x, dx) - u2_real)         #2阶
+        errors_2fdiff.append(dou_tri_diff(x, dx) - u2_real)        #1阶
 
     return errors_cdiff, errors_fdiff, errors_2cdiff, errors_2fdiff
 
 
 
 x=1.0
-deltax=np.linspace(1e-7,0.1,100)
+deltax=np.linspace(1e-7,0.1,10000)
 errors_cdiff, errors_fdiff, errors_2cdiff, errors_2fdiff = cal(x, deltax)
 
 data = np.column_stack((deltax, errors_cdiff, errors_fdiff, errors_2cdiff, errors_2fdiff))
 np.savetxt('errors_output_with_dx.txt', data,
            header='deltax   errors_cdiff   errors_fdiff   errors_2cdiff   errors_2fdiff')
 
-
+'''
+##32位精度
 x=np.float32(1.0)
 deltax=np.linspace(1e-7,0.1,100,dtype=np.float32)
 errors_cdiff, errors_fdiff, errors_2cdiff, errors_2fdiff = cal(x, deltax)
@@ -101,22 +94,17 @@ np.savetxt('errors_output_with_dx_float32.txt', data,
            header='deltax   errors_cdiff   errors_fdiff   errors_2cdiff   errors_2fdiff')
 print("精度=",deltax.dtype,errors_cdiff[3].dtype,errors_2fdiff[2].dtype)
 
-
+'''
 # 转换为 NumPy 数组（如果你用的是列表）
 errors_diff = np.array(errors_cdiff)
 errors_2diff = np.array(errors_2cdiff)
 
 
 
-# 第一阶导数误差图
-'''
-plt.plot(deltax, errors_diff, label='1st Derivative Error', color='blue')
-
-# 第二阶导数误差图
-#plt.plot(deltax, errors_2diff, label='2nd Derivative Error', color='red')
-
-
-
+# 二阶精度误差图
+plt.figure()  # 显式创建新图
+plt.plot(deltax, errors_cdiff, label='1st C-Derivative Error', color='blue')
+plt.plot(deltax, errors_2cdiff, label='2nd C-Derivative Error', color='red')
 # 添加图例、标题、标签等
 plt.title('Error vs. Δx')
 plt.xlabel('Δx (Step size)')
@@ -125,7 +113,20 @@ plt.legend()
 plt.grid(True)
 plt.tight_layout()
 plt.show()
-'''
+    
+    
+# 一阶精度误差图
+plt.figure()  # 显式创建新图
 
+plt.plot(deltax, errors_2fdiff, label='2nd F-Derivative Error', color='orange')
+plt.plot(deltax, errors_fdiff, label='1st F-Derivative Error', color='green')
+# 添加图例、标题、标签等
+plt.title('Error2 vs. Δx')
+plt.xlabel('Δx (Step size)')
+plt.ylabel('Error')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
     
 
