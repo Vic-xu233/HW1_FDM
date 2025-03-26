fe51b3f00c50587b00148b0231fd326333f166fe
Kiwi Xu <920650757@qq.com>
2025-03-26 23:44:39 +0800

    add

--- Q3/HW1Q3.py
+++ Q3/HW1Q3.py
@@ -81,30 +81,45 @@ def cal(x, deltax):
 
 
 x=1.0
-deltax=np.linspace(1e-7,0.1,int(1e4))
+deltax=np.linspace(1e-7,1e-3,int(1e4))
+start
 dx=deltax[1]-deltax[0]
-for f in range(1,5):
-    flag=f
+# 创建一个 2x2 的子图布局
+fig, axs = plt.subplots(2, 2, figsize=(14, 10))
+plt.rcParams['font.family'] = 'SimSun'  # 中文字体
+
+for idx, f in enumerate(range(1, 5)):
+    flag = f
     errors_cdiff, errors_fdiff, errors_2cdiff, errors_2fdiff = cal(x, deltax)
-    filename = f"errors_output_with_dx_flag{flag}.txt"
+    
+    # 保存对应数据
+    filename =f"新的errors_output_with_dx{dx}_flag{flag}.txt"
     data = np.column_stack((deltax, errors_cdiff, errors_fdiff, errors_2cdiff, errors_2fdiff))
     np.savetxt(filename, data,
                header='deltax   errors_cdiff   errors_fdiff   errors_2cdiff   errors_2fdiff')
-    
-    plt.rcParams['font.family'] = 'SimSun' ##中文
-    plt.plot(deltax, errors_2fdiff, label=f'1阶精度的二阶微分误差,步长={dx}', color='orange')
-    plt.plot(deltax, errors_fdiff, label=f'1阶精度的一阶微分误差,步长={dx}', color='green')
-    plt.plot(deltax, errors_cdiff, label=f'2阶精度一阶微分,步长={dx}', color='blue')
-    plt.plot(deltax, errors_2cdiff, label=f'2阶精度二阶微分,步长={dx}', color='red')
-    # 添加图例、标题、标签等
-    plt.title(f'误差随Δx变化（函数类型 flag={flag}）')
-    plt.xlabel('Δx (Step size)')
-    plt.ylabel('Error')
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig(f"error_with_function{flag}_with_dx={dx}.svg", bbox_inches='tight')
-    plt.show()
+
+    # 找到对应子图位置
+    ax = axs[idx // 2][idx % 2]
+    '''
+    ax.plot(deltax, errors_2fdiff, label='1阶精度 二阶微分', color='orange')
+    ax.plot(deltax, errors_fdiff, label='1阶精度 一阶微分', color='green')
+    ax.plot(deltax, errors_cdiff, label='2阶精度 一阶微分', color='blue')
+    ax.plot(deltax, errors_2cdiff, label='2阶精度 二阶微分', color='red')
+    '''
+    ax.loglog(deltax, errors_2fdiff, label='1阶精度 二阶微分', color='orange')
+    ax.loglog(deltax, errors_fdiff, label='1阶精度 一阶微分', color='green')
+    ax.loglog(deltax, errors_cdiff, label='2阶精度 一阶微分', color='blue')
+    ax.loglog(deltax, errors_2cdiff, label='2阶精度 二阶微分', color='red')
+    ax.set_title(f'flag = {flag}')
+    ax.set_xlabel('Δx')
+    ax.set_ylabel('errors')
+    ax.legend()
+    ax.grid(True)
+
+plt.suptitle(f"差分误差随 Δx 变化（4类函数对比)dx ≈ {dx:.2e}", fontsize=16)
+plt.tight_layout(rect=[0, 0, 1, 0.96])  # 给标题留空间
+plt.savefig(f"误差变化图,dx ≈ {dx:.2e}.svg", bbox_inches='tight')
+plt.show()
 
 '''
 ##32位精度
