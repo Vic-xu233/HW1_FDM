9b8c689da98c247ec4b2fa08b2042f6c6ac51123
Kiwi Xu <920650757@qq.com>
2025-03-26 17:00:07 +0800

    test1

--- hw1test.py
+++ hw1test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-flag=2  ##选择对哪个函数进行差分
+flag=4  ##选择对哪个函数进行差分
 k=2
 
 def u(x):##函数
@@ -9,8 +9,10 @@ def u(x):##函数
             u=sin(k*x)
         case 2:
             u=x**2
-        case _:
+        case 3:
             u=x**3
+        case _:
+            u=x
     return u
 def u_rdiff(x):##函数解析一阶导数
     match flag:
@@ -18,8 +20,10 @@ def u_rdiff(x):##函数解析一阶导数
             u=k*cos(k*x)
         case 2:
             u=2*x
-        case _:
+        case 3:
             u=3*x**2
+        case _:
+            u=1
     return u
 def u_2rdiff(x):##函数解析二阶导数
     match flag:
@@ -27,8 +31,10 @@ def u_2rdiff(x):##函数解析二阶导数
             u=-k*k*sin(k*x)
         case 2:
             u=2
-        case _:
+        case 3:
             u=6*x
+        case _:
+            u=0
     return u
 
 def f_diff(x,dx):                   ##前向差分一阶精度
@@ -55,34 +61,34 @@ def dou_tri_diff(x,dx):         ##四格点  1阶精度
     xi_1=x+dx
     xi_2=x+2*dx
     xi_n1=x-dx
-    u_dtcdiff=-u(xi_2)+4*u(xi_1)-5*u(x)-2*u(xi_n1)
+    u_dtcdiff=-u(xi_2)+4*u(xi_1)-5*u(x)+2*u(xi_n1)
     u_dtcdiff=u_dtcdiff/(dx*dx)
     return u_dtcdiff
 
+def cal(x, deltax):
+    errors_cdiff = []
+    errors_fdiff = []
+    errors_2cdiff = []
+    errors_2fdiff = []
+
+    for dx in deltax:
+        u1_real = u_rdiff(x)
+        u2_real = u_2rdiff(x)
+        errors_cdiff.append(c_diff(x, dx) - u1_real)
+        errors_fdiff.append(f_diff(x, dx) - u1_real)
+        errors_2cdiff.append(double_diff(x, dx) - u2_real)
+        errors_2fdiff.append(dou_tri_diff(x, dx) - u2_real)
+
+    return errors_cdiff, errors_fdiff, errors_2cdiff, errors_2fdiff
+
+
 x=1.0
 deltax=np.linspace(1e-7,0.1,100)
+errors_cdiff, errors_fdiff, errors_2cdiff, errors_2fdiff = cal(x, deltax)
 
-errors_cdiff=[]
-errors_fdiff=[]
-errors_2cdiff=[]
-errors_2fdiff=[]
-def cal(x,dx,deltax):
-    for dx in deltax:
-        u1_real=u_rdiff(x)
-        u2_real=u_2rdiff(x)
-        errors_cdiff.append(c_diff(x,dx)-u1_real)
-        errors_fdiff.append(f_diff(x,dx)-u1_real)
-        errors_2cdiff.append(double_diff(x,dx)-u2_real)
-        errors_2fdiff.append(dou_tri_diff(x,dx)-u2_real)
-    return 
-
-
-
-for i in range(len(errors_fdiff)):
-    print(errors_cdiff[i],"---",errors_fdiff[i])
-
-for i in range(len(errors_2fdiff)):
-    print(errors_2cdiff[i],"---",errors_2fdiff[i])
+data = np.column_stack((deltax, errors_cdiff, errors_fdiff, errors_2cdiff, errors_2fdiff))
+np.savetxt('errors_output_with_dx.txt', data,
+           header='deltax   errors_cdiff   errors_fdiff   errors_2cdiff   errors_2fdiff')
 
 
 # 转换为 NumPy 数组（如果你用的是列表）
